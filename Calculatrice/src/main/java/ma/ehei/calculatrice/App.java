450d02cb58f7e2b8567ab6b829aae7ed53ccff31
omar05m <omarkof99@gmail.com>
2023-12-31 16:38:10 +0100

    ana hbalt

--- Calculatrice/src/main/java/ma/ehei/calculatrice/App.java
+++ Calculatrice/src/main/java/ma/ehei/calculatrice/App.java
@@ -4,5 +4,6 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println(Calculatrice.add(5, 5));
 		System.out.println(Calculatrice.Multiplication(2, 3));
+		System.out.println("ana hmar");
 	}
 }
